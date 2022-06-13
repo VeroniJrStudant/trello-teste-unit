2afd90bd378983501835b4ec8901b27b9c9af021
VeroniJrStudant <veroni_vieira-jun1@estudante.sesisenai.org.br>
2022-06-13 00:30:13 -0300

    Exercicio feito

---
+++ Models/Produto.cs
@@ -0,0 +1,9 @@
+using System;
+namespace trello_semana3.Models
+{
+  public int id { get; set; }
+  public string nomeProduto { get; set; }
+  public decimal valorProduto { get; set; }
+  public DateTime dataCadastro { get; set; }
+  public DateTime dataCompra { get; set; }
+}
